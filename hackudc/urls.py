ea3777c16aa56cb689f3f5c043e0fc062abe294d
Delthia <git@delthia.com>
2025-09-22 23:34:53 +0200

    feat: Pruebas para la gestión de pases

--- hackudc/urls.py
+++ hackudc/urls.py
@@ -5,4 +5,9 @@ from hackudc import views
 urlpatterns = [
     path("", views.inicio, name="inicio"),
     path("registro", views.registro, name="registro"),
+    path("ruta", views.ruta, name="ruta"),
+    path("gestion", views.gestion, name="gestion"),
+    path("gestion/registro", views.alta, name="registro"),
+    path("gestion/pases", views.pases, name="pases"),
+    path("gestion/presencia", views.presencia, name="presencia")
 ]
--- hackudc/views.py
+++ hackudc/views.py
@@ -2,7 +2,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
-from hackudc.forms import ParticipanteForm
+from hackudc.forms import ParticipanteForm, Registro
+from hackudc.models import Participante
 
 
 # Create your views here.
@@ -21,3 +22,37 @@ def registro(request: HttpRequest):
         return HttpResponse("OK")
     else:
         return render(request, "registro.html", {"form": form})
+
+
+@require_http_methods(["GET"])
+def ruta(request: HttpRequest):
+    return HttpResponse("Nada por aquí")
+
+
+# /gestion/
+def gestion(request: HttpRequest):
+    return render(request, "gestion.html")
+
+
+def alta(request: HttpRequest):
+    form = Registro
+
+    if request.method == "POST":
+        form = Registro(request.POST)
+        if form.is_valid():
+            participante = Participante.objects.filter(correo=form.cleaned_data["persona"]).first()
+            participante.uuid = form.cleaned_data["acreditacion"]
+
+            participante.save()
+            return HttpResponse(participante.nombre+'-'+participante.talla_camiseta)
+
+
+    return render(request, "gestion-registro.html", {"form": form})
+
+
+def pases(request: HttpRequest):
+    return render(request, "pases.html")
+
+
+def presencia(request: HttpRequest):
+    return render(request, "presencia.html")
